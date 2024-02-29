2a35c77249b94de6539359d6d569b0c9864a633d
Juan-Claudio <angeelric@gmail.com>
2024-02-29 17:14:38 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import React from 'react';
+
+export default class App extends React.Component
+{
+   render()
+   {
+      return (
+         <div className="App"></div>         
+      );
+   }
+}
