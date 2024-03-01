23c2e7a81dfaa9b2ae67d64a24de663021d39361
Juan-Claudio <angeelric@gmail.com>
2024-03-01 18:35:17 +0100

    Incorporation of resources and modification of App.js to display them.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import '../styles/App.css';
+import projects from '../resources/projects_list.json'
 import React from 'react';
+import IdCard from './IdCard';
 
 export default class App extends React.Component
 {
    render()
    {
       return (
-         <div className="App"></div>         
+         <div className='App'>
+            <header>
+                <h1>Portfolio of Juan-Claudio</h1>
+            </header>
+            <main className="App-main">
+               {projects.map((project, idx)=>{
+                  return (
+                     <IdCard key={idx} projectInfo={project} />
+                  )
+               })}
+            </main>
+         </div>
       );
    }
 }
